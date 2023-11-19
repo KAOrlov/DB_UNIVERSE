747a709f3ecb209064c8011c5059318cac21f304
Evalti <valentina.evstratova@yandex.ru>
2023-11-19 21:05:33 +0300

    изменены параметры связей

--- Tabels(DataDefinitionLanguage)/tb_cat_values.sql
+++ Tabels(DataDefinitionLanguage)/tb_cat_values.sql
@@ -3,17 +3,9 @@ CREATE TABLE tb_cat_values (
     id_property INT NOT NULL,
     value_prop VARCHAR(50) NOT NULL,
     FOREIGN KEY (id_object) REFERENCES DB_UNIVERSE.void.tb_planets (id)
-        ON DELETE CASCADE
-        ON UPDATE CASCADE
     FOREIGN KEY (id_object) REFERENCES DB_UNIVERSE.void.tb_ stars (id)
-        ON DELETE CASCADE
-        ON UPDATE CASCADE
     FOREIGN KEY (id_object) REFERENCES DB_UNIVERSE.void.tb_stars_system (id)
-        ON DELETE CASCADE
-        ON UPDATE CASCADE
-    FOREIGN KEY (id_property) REFERENCES DB_UNIVERSE.void.tb_cat_properties (id)
-        ON DELETE CASCADE
-        ON UPDATE CASCADE   
+    FOREIGN KEY (id_property) REFERENCES DB_UNIVERSE.void.tb_cat_properties (id) 
 )
 
 
