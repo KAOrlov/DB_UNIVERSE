86900c232b4e72475d8859100716872d3e87196a
Evstratova <Evstratova.V@citilink.ru>
2023-12-10 12:56:54 +0300

    исправлены ключи

--- Tabels(DataDefinitionLanguage)/tb_planet_system.sql
+++ Tabels(DataDefinitionLanguage)/tb_planet_system.sql
@@ -1,9 +1,9 @@
 CREATE TABLE DB_UNIVERSE.void.tb_planet_system (
-    id INT IDENTITY(1,1) PRIMARY KEY NOT NULL, 
     planet_id INT NOT NULL,
     star_id INT NOT NULL,
     galaxy_id INT NOT NULL
 
+    CONSTRAINT PK_planet_system PRIMARY KEY(planet_id, star_id, galaxy_id)
     FOREIGN KEY (galaxy_id) REFERENCES DB_UNIVERSE.void.tb_galaxy (id)
     FOREIGN KEY (planet_id) REFERENCES DB_UNIVERSE.void.tb_planets (id)
     FOREIGN KEY (star_id) REFERENCES DB_UNIVERSE.void.tb_stars (id)
--- Tabels(DataDefinitionLanguage)/tb_stars_galaxy.sql
+++ Tabels(DataDefinitionLanguage)/tb_stars_galaxy.sql
@@ -1,8 +1,8 @@
 CREATE TABLE DB_UNIVERSE.void.tb_stars_galaxy (
-    id INT IDENTITY(1,1) PRIMARY KEY NOT NULL, 
     galaxy_id INT NOT NULL,
-    star_id INT NOT NULL,
+    star_id INT NOT NULL
     
+    CONSTRAINT PK_galaxy_star PRIMARY KEY(galaxy_id, star_id)
     FOREIGN KEY (galaxy_id) REFERENCES DB_UNIVERSE.void.tb_galaxy (id)
     FOREIGN KEY (star_id) REFERENCES DB_UNIVERSE.void.tb_stars (id)
 )
